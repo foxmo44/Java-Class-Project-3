b18087af70663138a803a2f74944a7efa10be26e
foxmo <foxmo44@yahoo.com>
2016-09-25 14:29:11 -0500

    Initial addition of Project 3 to Git and Github

---
+++ src/Project03Fox.java
@@ -0,0 +1,18 @@
+// Author: Michael Fox
+// Session: Advanced Java
+// Project: Project 3
+// Current Date: 9/25/2016
+// DateDue: 1799.01.01
+public class Project03Fox
+{
+    /**
+     * This the main function that runs at the start
+     * @param args - input arguments from the command line
+     */
+    static public void main(String[] args)
+    {
+        System.out.println("Project Place Holder");
+    }
+
+
+}
